54d7a8b5bbc613b52946bb7169aa53fba87784d2
bryanbuiles <1482@holbertonschool.com>
2020-08-17 17:40:24 -0500

    fix 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""Fab script"""
-import os
+"""
+Fabric script
+"""
+
 from datetime import datetime
 from fabric.api import local
+from os.path import isdir
 
 
 def do_pack():
-    """Packs web_static into tgz"""
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = "versions/web_static_" + date + ".tgz"
-    if os.isdir("versions") is False:
-        local("mkdir -p versions")
-    local("tar -cvzf " + file_name + " web_static")
-    if os.path.exists(file_name):
+    """tbz"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    else:
+    except:
         return None
